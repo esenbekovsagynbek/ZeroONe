89b621829d863caa02c67b414fb196d9b4e1df71
elemanjan <mr.eleman@gmail.com>
2021-04-26 18:53:06 +0600

    all block designed

--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
+import datetime
 from django.shortcuts import render
 from .models import Courses
 
 
+today = datetime.datetime.now()
+month = today.strftime("%B")
+year = today.strftime("%Y")
+
+
 def home(request):
     courses = Courses.objects.all()
+    print(courses[0].image.url)
     return render(request, 'main/index.html', {
         'courses': courses
     })
+
+
